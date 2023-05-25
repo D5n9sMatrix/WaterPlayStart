a028b95c649fa170a093021e6e6bb8a98d3430b6
D5n9sMatrix <denis.02live@hotmail.com>
2023-05-25 15:57:30 -0300

    welcome Water Play Start Math

---
+++ matrix/gnu/lib/Obas.java
@@ -0,0 +1,8 @@
+public static void Tools(String[]args){
+private static final String KEY_number="number";
+// TODO: 25/05/2023
+public static void getInstance(){
+        return true;
+        }
+private static final int Tools=801;
+        }
